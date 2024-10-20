cbd8245a4df4ae725636d76d91d924965c26e767
PinappleUnderTheSea <1411493875@qq.com>
2024-10-20 16:51:06 +0800

    implement handlers

--- src/contract.ts
+++ src/contract.ts
@@ -11,6 +11,9 @@ import {
   Transfer
 } from "../generated/schema"
 
+import { MintCall } from "../generated/Contract/Contract";
+import { Token } from "../generated/schema";
+
 export function handleApproval(event: ApprovalEvent): void {
   let entity = new Approval(
     event.transaction.hash.concatI32(event.logIndex.toI32())
@@ -70,4 +73,19 @@ export function handleTransfer(event: TransferEvent): void {
   entity.transactionHash = event.transaction.hash
 
   entity.save()
+}
+
+
+export function handleMint(call: MintCall): void {
+  let tokenId = call.inputs.tokenId;
+  let creator = call.from;
+  let metadataURI = call.inputs.tokenURI;
+
+  let token = new Token(tokenId.toString());
+  token.tokenId = tokenId;
+  token.creator = creator;
+  token.metadataURI = metadataURI;
+  token.owner = creator;
+
+  token.save();
 }
